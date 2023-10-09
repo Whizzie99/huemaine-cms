63a2c786f703c1b3b4bc22cd7f76bdd21835b185
whizzie99 <wisdomobiomadev@gmail.com>
2023-10-09 02:06:22 +0100

    create app scaffold and schemas

---
+++ schemas/post.ts
@@ -0,0 +1,55 @@
+export default {
+  name: 'post',
+  type: 'document',
+  title: 'Post',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      options: {
+        source: 'title',
+      },
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+    },
+    {
+      name: 'content',
+      type: 'array',
+      title: 'Content',
+      of: [
+        {
+          type: 'block',
+        },
+        {
+          type: 'image',
+          fields: [
+            {
+              type: 'text',
+              name: 'alt',
+              title: 'Alternative Text',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'author',
+      type: 'string',
+      title: 'Author',
+    },
+    {
+      name: 'date',
+      type: 'date',
+      title: 'Date',
+    },
+  ],
+}
---
+++ schemas/project.ts
@@ -0,0 +1,27 @@
+export default {
+  name: 'project',
+  type: 'document',
+  title: 'Project',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'subtitle',
+      type: 'string',
+      title: 'Subtitle',
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+    },
+    {
+      name: 'url',
+      type: 'url',
+      title: 'url',
+    },
+  ],
+}
